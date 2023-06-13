6b31b6a81bebd17298e666e7bc0ae5bd89cb682d
vjsmit <vj.smit97@gmail.com>
2023-06-13 21:46:16 +0530

    modules

--- 7-module1/ec2/main.tf
+++ 7-module1/ec2/main.tf
@@ -3,18 +3,18 @@ resource "aws_instance" "web" {
   instance_type = "t3.micro"
   vpc_security_group_ids = [aws_security_group.sg.id]
   tags = {
-    Name = var.name
+    Name = var.names
   }
 }
 
 data "aws_ami" "example" {
   owners      = ["973714476881"]
   most_recent = true
-  name_regex  = "Centos-8-DevOps-Practice"
+  names_regex  = "Centos-8-DevOps-Practice"
 }
 
 resource "aws_security_group" "sg" {
-  name        = var.name
+  names        = var.names
   description = "Allow TLS inbound traffic"
 
   ingress {
@@ -34,8 +34,8 @@ resource "aws_security_group" "sg" {
   }
 
   tags = {
-    Name = var.name
+    Name = var.names
   }
 }
 
-variable "name" {}
+variable "names" {}
