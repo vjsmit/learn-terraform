f22817d0738d000ff8ebf00b691be5cbbf0410ac
vjsmit <vj.smit97@gmail.com>
2023-06-17 12:15:06 +0530

    provisioners

--- 12-functions/mian.tf
+++ 12-functions/mian.tf
@@ -5,3 +5,11 @@ variable "class" {
 output "class" {
   value = upper(var.class)
 }
+
+variable "fruits" {
+  default = [ "apple", "banana", "mango" ]
+}
+
+output "fruits" {
+  value = length(var.fruits)
+}
