424dfa8ad14db8e662c986050ad7724223b382f1
vjsmit <vj.smit97@gmail.com>
2023-06-17 12:22:04 +0530

    provisioners

--- 12-functions/mian.tf
+++ 12-functions/mian.tf
@@ -13,3 +13,24 @@ variable "fruits" {
 output "fruits" {
   value = length(var.fruits)
 }
+
+variable "classes" {
+  default = {
+    devops = {
+      name = "devops"
+      time = "8 AM"
+      topics = [ "shell", "python" ]
+    }
+    aws = {
+      name = "aws"
+    }
+  }
+}
+
+output "classes" {
+  value = var.classes.devops.topics
+}
+
+output "classes" {
+  value = var.classes[aws][topics]
+}
